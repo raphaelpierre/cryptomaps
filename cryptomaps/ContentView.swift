ce935d4b887b8744e3922796cad9838f506fcd80
Raphaël Pierre <pierre.raphael@gmail.com>
2025-03-15 12:46:32 +0100

    Initial Commit

---
+++ cryptomaps/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cryptomaps
+//
+//  Created by Raphael PIERRE on 15.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cryptomaps/cryptomapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  cryptomapsApp.swift
+//  cryptomaps
+//
+//  Created by Raphael PIERRE on 15.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct cryptomapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
